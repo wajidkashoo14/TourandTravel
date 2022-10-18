a2150775e572636b28bd339accded652751af463
wajidkashoo14 <wajidkashoo14@gmail.com>
2022-10-19 00:24:42 +0530

    added signup

--- components/Contact.js
+++ components/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
   return (
     <Container maxW={"80%"} height={"80vh"} bg={"#fff"}>
       <Box py={"6"} borderBottom={"2px solid #E5E5E5"}>
-        <Heading className={styles.heading}>{`Let's Get in Touch!`}</Heading>
+        <Heading className={styles.heading}>{` Get in Touch!`}</Heading>
         <Text>Get Assistance From Expert Tour Advisors!</Text>
       </Box>
       <Box display={"flex"} flexDir={"column"} gap={"5"} py={"10"}>
