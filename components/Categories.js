b300d4d83ceb6fabe7398a3cf8c060ae8a577b4e
wajidkashoo14 <wajidkashoo14@gmail.com>
2022-11-15 22:22:08 +0530

    made changes with home page

--- components/Categories.js
+++ components/Categories.js
@@ -5,49 +5,47 @@ import { AiOutlineHeart } from "react-icons/ai";
 
 function Categories({ type, img }) {
   return (
-    <Container>
-      <Box
-        position={"relative"}
-        borderRadius={"8px"}
-        cursor={"pointer"}
-        overflow={"hidden"}
-        transition={"all 0.2s"}
-        minH={"400px"}
-        maxW={"550px"}
+    <Box
+      position={"relative"}
+      borderRadius={"8px"}
+      cursor={"pointer"}
+      overflow={"hidden"}
+      transition={"all 0.2s"}
+      minH={"400px"}
+      maxW={"550px"}
+    >
+      <Image
+        src={img}
+        objectFit={"cover"}
+        h={{ base: "100%", md: "400px", lg: "350px" }}
+        loading={"lazy"}
+      />
+      <Heading
+        position={"absolute"}
+        top={"70%"}
+        left={"5%"}
+        fontSize={"2xl"}
+        color={"#fff"}
+        fontWeight={"700"}
       >
-        <Image
-          src={img}
-          objectFit={"cover"}
-          h={{ base: "100%", md: "400px", lg: "350px" }}
-          loading={"lazy"}
-        />
-        <Heading
-          position={"absolute"}
-          top={"70%"}
-          left={"5%"}
-          fontSize={"2xl"}
-          color={"#fff"}
-          fontWeight={"700"}
-        >
-          {type}
-        </Heading>
-        <Box position={"absolute"} top={"10%"} right={"5%"}>
-          <AiOutlineHeart fontSize={"1.5rem"} color={"#fff"} />
-        </Box>
-        <Box
-          position={"absolute"}
-          bottom={"15%"}
-          left={"5%"}
-          display={"flex"}
-          gap={"3"}
-        >
-          <GoLocation fontSize={"1.5rem"} color={"#fff"} />
-          <Text color={"#fff"} fontSize={"1.3rem"}>
-            Kashmir
-          </Text>
-        </Box>
+        {type}
+      </Heading>
+      <Box position={"absolute"} top={"10%"} right={"5%"}>
+        <AiOutlineHeart fontSize={"1.5rem"} color={"#fff"} />
       </Box>
-    </Container>
+      <Box
+        position={"absolute"}
+        bottom={"15%"}
+        left={"5%"}
+        display={"flex"}
+        gap={"3"}
+      >
+        <GoLocation fontSize={"1.5rem"} color={"#fff"} />
+        <Text color={"#fff"} fontSize={"1.3rem"}>
+          Kashmir
+        </Text>
+      </Box>
+    </Box>
   );
 }
 
--- components/Tourcategories.js
+++ components/Tourcategories.js
@@ -1,11 +1,18 @@
-import { Box, Container, Heading } from "@chakra-ui/react";
+import { Box, Container, Heading, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import Categories from "./Categories";
 import styles from "../styles/discription.module.css";
+import { GoLocation } from "react-icons/go";
+import { AiOutlineHeart } from "react-icons/ai";
 
 function Tourcategories() {
   return (
-    <Container maxW={"100%"} backgroundColor={"#F7F7F7"} px={"5rem"}>
+    <Container
+      maxW={"100%"}
+      backgroundColor={"#F7F7F7"}
+      px={"5rem"}
+      py={"8rem"}
+    >
       <Heading textAlign={"center"} my={"8"} className={styles.heading}>
         Tour categories
       </Heading>
@@ -18,15 +25,263 @@ function Tourcategories() {
         alignItems={"center"}
         mt={"5rem"}
       >
-        <Categories type={"Kashmir tour pakages"} img={"/tourone.jpg"} />
-        <Categories type={"Ladakh tour pakages"} img={"/tourtwo.jpg"} />
-        {/* <Categories type={"Couple tour pakages"} img={"/tourthree.jpg"} />
-        <Categories type={"Adventure tour pakages"} img={"/tourfour.jpg"} />
-        <Categories type={"Spiritual tour pakages"} img={"/tourseven.jpg"} /> */}
-        {/* <Categories
-          type={"Skiing/Snowboarding tour pakages"}
-          img={"/toureight.jpg"}
-        /> */}
+        <Flex maxWidth={"100%"} gap={"8"}>
+          <Box
+            position={"relative"}
+            borderRadius={"8px"}
+            cursor={"pointer"}
+            overflow={"hidden"}
+            transition={"all 0.2s"}
+            h={"400px"}
+            w={"600px"}
+          >
+            <Image
+              src="./home.jpg"
+              h={"400px"}
+              w={"600px"}
+              loading={"lazy"}
+              borderRadius={"8px"}
+            />
+            <Heading
+              position={"absolute"}
+              top={"70%"}
+              left={"5%"}
+              fontSize={"2xl"}
+              color={"#fff"}
+              fontWeight={"700"}
+            >
+              Kashmir
+            </Heading>
+            <Box position={"absolute"} top={"10%"} right={"5%"}>
+              <AiOutlineHeart fontSize={"1.5rem"} color={"#fff"} />
+            </Box>
+            <Box
+              position={"absolute"}
+              bottom={"15%"}
+              left={"5%"}
+              display={"flex"}
+              gap={"3"}
+            >
+              <GoLocation fontSize={"1.5rem"} color={"#fff"} />
+              <Text color={"#fff"} fontSize={"1.3rem"}>
+                Kashmir
+              </Text>
+            </Box>
+          </Box>
+          <Box
+            position={"relative"}
+            borderRadius={"8px"}
+            cursor={"pointer"}
+            overflow={"hidden"}
+            transition={"all 0.2s"}
+            h={"400px"}
+            w={"600px"}
+          >
+            <Image
+              src="./tourone.jpg"
+              h={"400px"}
+              w={"600px"}
+              loading={"lazy"}
+              borderRadius={"8px"}
+            />
+            <Heading
+              position={"absolute"}
+              top={"70%"}
+              left={"5%"}
+              fontSize={"2xl"}
+              color={"#fff"}
+              fontWeight={"700"}
+            >
+              Kashmir
+            </Heading>
+            <Box position={"absolute"} top={"10%"} right={"5%"}>
+              <AiOutlineHeart fontSize={"1.5rem"} color={"#fff"} />
+            </Box>
+            <Box
+              position={"absolute"}
+              bottom={"15%"}
+              left={"5%"}
+              display={"flex"}
+              gap={"3"}
+            >
+              <GoLocation fontSize={"1.5rem"} color={"#fff"} />
+              <Text color={"#fff"} fontSize={"1.3rem"}>
+                Kashmir
+              </Text>
+            </Box>
+          </Box>
+        </Flex>
+
+        <Box
+          position={"relative"}
+          borderRadius={"8px"}
+          cursor={"pointer"}
+          overflow={"hidden"}
+          transition={"all 0.2s"}
+          minH={"400px"}
+          maxW={"1380px"}
+        >
+          <Image
+            src="./tourtwo.jpg"
+            h={"400px"}
+            w={"1240px"}
+            loading={"lazy"}
+            borderRadius={"8px"}
+          />
+          <Heading
+            position={"absolute"}
+            top={"70%"}
+            left={"5%"}
+            fontSize={"2xl"}
+            color={"#fff"}
+            fontWeight={"700"}
+          >
+            Kashmir
+          </Heading>
+          <Box position={"absolute"} top={"10%"} right={"5%"}>
+            <AiOutlineHeart fontSize={"1.5rem"} color={"#fff"} />
+          </Box>
+          <Box
+            position={"absolute"}
+            bottom={"15%"}
+            left={"5%"}
+            display={"flex"}
+            gap={"3"}
+          >
+            <GoLocation fontSize={"1.5rem"} color={"#fff"} />
+            <Text color={"#fff"} fontSize={"1.3rem"}>
+              Kashmir
+            </Text>
+          </Box>
+        </Box>
+        <Flex gap={"10"}>
+          <Box
+            position={"relative"}
+            borderRadius={"8px"}
+            cursor={"pointer"}
+            overflow={"hidden"}
+            transition={"all 0.2s"}
+            h={"400px"}
+            w={"390px"}
+          >
+            <Image
+              src="./tourthree.jpg"
+              h={"400px"}
+              w={"600px"}
+              loading={"lazy"}
+              borderRadius={"8px"}
+            />
+            <Heading
+              position={"absolute"}
+              top={"70%"}
+              left={"5%"}
+              fontSize={"2xl"}
+              color={"#fff"}
+              fontWeight={"700"}
+            >
+              Kashmir
+            </Heading>
+            <Box position={"absolute"} top={"10%"} right={"5%"}>
+              <AiOutlineHeart fontSize={"1.5rem"} color={"#fff"} />
+            </Box>
+            <Box
+              position={"absolute"}
+              bottom={"15%"}
+              left={"5%"}
+              display={"flex"}
+              gap={"3"}
+            >
+              <GoLocation fontSize={"1.5rem"} color={"#fff"} />
+              <Text color={"#fff"} fontSize={"1.3rem"}>
+                Kashmir
+              </Text>
+            </Box>
+          </Box>
+          <Box
+            position={"relative"}
+            borderRadius={"8px"}
+            cursor={"pointer"}
+            overflow={"hidden"}
+            transition={"all 0.2s"}
+            h={"400px"}
+            w={"390px"}
+          >
+            <Image
+              src="./tourfour.jpg"
+              h={"400px"}
+              w={"600px"}
+              loading={"lazy"}
+              borderRadius={"8px"}
+            />
+            <Heading
+              position={"absolute"}
+              top={"70%"}
+              left={"5%"}
+              fontSize={"2xl"}
+              color={"#fff"}
+              fontWeight={"700"}
+            >
+              Kashmir
+            </Heading>
+            <Box position={"absolute"} top={"10%"} right={"5%"}>
+              <AiOutlineHeart fontSize={"1.5rem"} color={"#fff"} />
+            </Box>
+            <Box
+              position={"absolute"}
+              bottom={"15%"}
+              left={"5%"}
+              display={"flex"}
+              gap={"3"}
+            >
+              <GoLocation fontSize={"1.5rem"} color={"#fff"} />
+              <Text color={"#fff"} fontSize={"1.3rem"}>
+                Kashmir
+              </Text>
+            </Box>
+          </Box>
+          <Box
+            position={"relative"}
+            borderRadius={"8px"}
+            cursor={"pointer"}
+            overflow={"hidden"}
+            transition={"all 0.2s"}
+            h={"400px"}
+            w={"390px"}
+          >
+            <Image
+              src="./tourfive.jpg"
+              h={"400px"}
+              w={"600px"}
+              loading={"lazy"}
+              borderRadius={"8px"}
+            />
+            <Heading
+              position={"absolute"}
+              top={"70%"}
+              left={"5%"}
+              fontSize={"2xl"}
+              color={"#fff"}
+              fontWeight={"700"}
+            >
+              Kashmir
+            </Heading>
+            <Box position={"absolute"} top={"10%"} right={"5%"}>
+              <AiOutlineHeart fontSize={"1.5rem"} color={"#fff"} />
+            </Box>
+            <Box
+              position={"absolute"}
+              bottom={"15%"}
+              left={"5%"}
+              display={"flex"}
+              gap={"3"}
+            >
+              <GoLocation fontSize={"1.5rem"} color={"#fff"} />
+              <Text color={"#fff"} fontSize={"1.3rem"}>
+                Kashmir
+              </Text>
+            </Box>
+          </Box>
+        </Flex>
       </Box>
     </Container>
   );
